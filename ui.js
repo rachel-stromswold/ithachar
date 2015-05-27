2f746ae1c29b8aa747635a0a9b87b4f8cca4da68
Samuel Stromswold <samstromsw@gmail.com>
2015-05-27 11:41:44 -0400

    Added fields for touch AC, flat footed AC, Damage Reduction, and Initiative Modifiers. Also added automatic calculators for Touch and Flat Footed.

--- ui.js
+++ ui.js
@@ -26,9 +26,18 @@ function collapseField(field){
 
 function updateAC() {
 	var ac = 10;
+	var t_ac=10;//touch AC
+	var f_ac=10;//flat footed AC
+
 	var acMods = document.getElementsByClassName('acMod');
 	for(var i = 0; i < acMods.length; ++i) {
 		ac += +(acMods[i].value);
+		if(!(i==0 || i==1 || i==4))//If we are looking at armor (artificial or natural) it is ignored for touch attacks
+			t_ac+=+(acMods[i].value);
+		if(!(i==2 || i==5))//TODO:Check for feats that affect this, for example uncanny dodge
+			f_ac+=+(acMods[i].value);
 	}
 	document.getElementById('ac0').value=ac;
+	document.getElementById('ac8').value=t_ac;
+	document.getElementById('ac9').value=f_ac;
 }
